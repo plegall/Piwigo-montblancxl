eb952d4af4a6e2d3ed030d1bdcf5f42d061ae88c
patdenice <pat@piwigo.org>
2010-03-29 16:58:28 +0000

    Convert language file to utf8
    git-svn-id: http://piwigo.org/svn/extensions/montblancxl@5450 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fr_FR/theme.lang.php
+++ language/fr_FR/theme.lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['Display page banner on home page'] = 'Afficher la bannière sur la page d\'accueil';
-$lang['Display page banner on categories pages'] = 'Afficher la bannière sur la page des catégories';
-$lang['Display page banner on picture page'] = 'Afficher la bannière sur la page des photos';
-$lang['Display page banner on other pages'] = 'Afficher la bannière sur les autres pages';
+$lang['Display page banner on home page'] = 'Afficher la banniÃ¨re sur la page d\'accueil';
+$lang['Display page banner on categories pages'] = 'Afficher la banniÃ¨re sur la page des catÃ©gories';
+$lang['Display page banner on picture page'] = 'Afficher la banniÃ¨re sur la page des photos';
+$lang['Display page banner on other pages'] = 'Afficher la banniÃ¨re sur les autres pages';
 
 ?>
