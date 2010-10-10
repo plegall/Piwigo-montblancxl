22af735b75571ecb71fa4de6eebd67c3fc67e8f7
ddtddt <ddtddt@piwigo.org>
2010-10-10 13:07:12 +0000

    [extension] montblancxl thanks to esrgio / add IT
    git-svn-id: http://piwigo.org/svn/extensions/montblancxl@7144 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Visualizza banner nella home page'] = 'Visualizza banner nella home page';
+$lang['Visualizza banner nelle pagina di categoria'] = 'Visualizza banner nelle pagina di categoria';
+$lang['Visualizza banner nelle pagine delle foto'] = 'Visualizza banner nelle pagine delle foto';
+$lang['Visualizza banner nelle altre pagine'] = 'Visualizza banner nelle altre pagine';
+
+?>
