a0c626c21af0f3adc4d62678ff63ea936b66ea7d
riopwg <riopwg@piwigo.org>
2010-11-07 17:06:11 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/montblancxl@7678 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/theme.lang.php
+++ language/it_IT/theme.lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['Visualizza banner nella home page'] = 'Visualizza il banner sulla pagina principale';
-$lang['Visualizza banner nelle pagina di categoria'] = 'Visualizza il banner sulla pagina delle categorie';
-$lang['Visualizza banner nelle pagine delle foto'] = 'Visualizza il banner sulla pagine delle foto';
-$lang['Visualizza banner nelle altre pagine'] = 'Visualizza banner sulle altre pagine';
+$lang['Display page banner on home page'] = 'Visualizza il banner sulla pagina principale';
+$lang['Display page banner on categories pages'] = 'Visualizza il banner sulla pagina delle categorie';
+$lang['Display page banner on picture page'] = 'Visualizza il banner sulla pagine delle foto';
+$lang['Display page banner on other pages'] = 'Visualizza banner sulle altre pagine';
 
 ?>
