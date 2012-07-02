d526b733131e68f56451e36261a45b6eba640188
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-02 00:05:16 +0000

    [montblanc_xl] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/montblancxl@16258 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/theme.lang.php
+++ language/tr_TR/theme.lang.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang['Display page banner on home page'] = 'Sayfa başlığını ana sayfada göster';
-$lang['Display page banner on categories pages'] = 'sayfa başlığını kategori sayfalarında göster';
-$lang['Display page banner on picture page'] = 'sayfa başlığını fotoğraf sayfalarında göster';
-$lang['Display page banner on other pages'] = 'sayfa başlığını diğer sayfalarda göster';
+$lang['Display page banner on categories pages'] = 'Sayfa başlığını kategori sayfalarında göster';
+$lang['Display page banner on picture page'] = 'Sayfa başlığını fotoğraf sayfalarında göster';
+$lang['Display page banner on other pages'] = 'Sayfa başlığını diğer sayfalarda göster';
 
 ?>
