97cc407eae0545b4463c1743fa606fbbbbedd502
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-16 06:22:24 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/theme.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Birta síðuborða á flokkasíðum';
+$lang['Display page banner on home page'] = 'Birta síðuborða á heimasíðunni';
+$lang['Display page banner on other pages'] = 'Birta síðuborða á öðrum síðum';
+$lang['Display page banner on picture page'] = 'Birta síðuborða á myndasíðu';
