6a43a6d6ad4ef89d50c13882d225d024d0f60b0a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-04-21 09:51:18 +0200

    Update ro_RO, thanks to adi_nicola

---
+++ language/ro_RO/theme.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Afiseaza bannerul paginii in paginile "categorii"';
+$lang['Display page banner on home page'] = 'Afiseaza bannerul paginii in pagina "acasa"';
+$lang['Display page banner on other pages'] = 'Afiseaza bannerul paginii in paginile "alte"';
+$lang['Display page banner on picture page'] = 'Afiseaza bannerul paginii in pagina "imagine"';
