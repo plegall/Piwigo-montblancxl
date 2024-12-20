8917ab9c706ed45df692701a256ebed60d0736cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:53:54 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/theme.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'הצג באנר עמוד בעמודי קטגוריות';
+$lang['Display page banner on home page'] = 'הצג באנר עמוד בעמוד הבית';
+$lang['Display page banner on other pages'] = 'הצג באנר עמוד בעמודים אחרים';
+$lang['Display page banner on picture page'] = 'הצג באנר עמוד בעמוד התמונה';
